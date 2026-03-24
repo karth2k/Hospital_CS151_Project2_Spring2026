5320712c732eda5b172d8576ab2162df1a5613ff
fqh666a <qingheng.fang@gmail.com>
2026-03-24 10:42:29 -0700

    added getters, setters, and toString to Pharmacy

--- src/main/java/hospital/Pharmacy.java
+++ src/main/java/hospital/Pharmacy.java
@@ -28,4 +28,53 @@ public class Pharmacy {
         this.medicinePrices = new double[MAX_OBJECTS];
         this.medicineCount = 0;
     }
+
+    // -------------------------------------------------------------------------
+    // Getters & Setters
+    // -------------------------------------------------------------------------
+
+    public String getPharmacyId() {
+        return pharmacyId;
+    }
+
+    public void setPharmacyId(String pharmacyId) {
+        if (pharmacyId != null && !pharmacyId.isBlank()) {
+            this.pharmacyId = pharmacyId;
+        }
+    }
+
+    public int getMedicineCount() {
+        return medicineCount;
+    }
+
+    /**
+     * Updates the price of an existing medicine.
+     *
+     * @param medicine name of the medicine
+     * @param price    new unit price
+     * @return true if updated, false if medicine not found
+     */
+    public boolean setMedicinePrice(String medicine, double price) {
+        for (int i = 0; i < medicineCount; i++) {
+            if (medicineNames[i].equalsIgnoreCase(medicine)) {
+                medicinePrices[i] = price;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // -------------------------------------------------------------------------
+    // toString
+    // -------------------------------------------------------------------------
+
+    /**
+     * Returns a summary of the pharmacy including ID and medicine count.
+     *
+     * @return formatted pharmacy summary string
+     */
+    @Override
+    public String toString() {
+        return "Pharmacy{id='" + pharmacyId + "', medicineCount=" + medicineCount + "}";
+    }
 }
