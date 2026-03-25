10de415f3e8bca2292fd58b5602a0bf178343b81
Karthik <karthikmuthukumar05@gmail.com>
2026-03-25 12:55:53 -0700

    Added attributes and counters into hospital class, added small comment block in billable interface.

--- src/main/java/hospital/Billable.java
+++ src/main/java/hospital/Billable.java
@@ -1,6 +1,10 @@
 package hospital;
 
 public interface Billable {
+    /*
+     * Class Implemented by Karthik Muthukumar
+     * ID: 016281915
+     */
 
     //Adds charge to patients bill
     void addCharge(double amount);
--- src/main/java/hospital/Hospital.java
+++ src/main/java/hospital/Hospital.java
@@ -1,6 +1,27 @@
 package hospital;
 
 public class Hospital {
+    /*
+     * Class Implemented by Karthik Muthukumar
+     * ID: 016281915
+     */
 
+    //Max objects limit
     public static final int MAX_OBJECTS = 100;
+
+    //Attributes
+    private String hospitalName;
+    private Patient[] patients;
+    private Doctor[] doctors;
+    private Nurse[] nurses;
+    private Appointment[] appointments;
+    private Pharmacy pharmacy;
+
+    //Counters
+    private int patientCount;
+    private int doctorCount;
+    private int nurseCount;
+    private int appointmentCount;
+
+
 }
