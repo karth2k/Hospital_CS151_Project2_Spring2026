d96bb2d0c43fedf6df2f31e5f5285f8798ae7210
Karthik <karthikmuthukumar05@gmail.com>
2026-03-25 13:11:16 -0700

    Added the getters for hospital class

--- src/main/java/hospital/Hospital.java
+++ src/main/java/hospital/Hospital.java
@@ -23,5 +23,47 @@ public class Hospital {
     private int nurseCount;
     private int appointmentCount;
 
+    //Getters
+    public String getHospitalName(){
+        //gets the name of the hospital
+        return hospitalName;
+    }
+    public Patient[] getPatients() {
+        //Gets list of patients
+        return patients;
+    }
+    public Doctor[] getDoctors(){
+        //Gets list of doctors
+        return doctors;
+    }
+    public Nurse[] getNurses(){
+        //Gets list of nurses
+        return nurses;
+    }
+    public Appointment[] getAppointments(){
+        //Gets list of appointments
+        return appointments;
+    }
+    public Pharmacy getPharmacy(){
+        //Gets pharmacy
+        return pharmacy;
+    }
+    public int getPatientCount(){
+        //Gets patient count
+        return patientCount;
+    }
+    public int getDoctorCount(){
+        //Gets doctor count
+        return doctorCount;
+    }
+    public int getNurseCount(){
+        //Gets nurse count
+        return nurseCount;
+    }
+    public int getAppointmentCount(){
+        //Gets appointment count
+        return appointmentCount;
+    }
+
 
 }
