ad75242c1fee3b8988665c0cceafcdd2b4b577e9
Karthik <karthikmuthukumar05@gmail.com>
2026-03-28 14:24:59 -0700

    added tests for clockout, updatesalary, assigndepartment, and tostring stafftests finished

--- src/test/java/hospital/StaffTest.java
+++ src/test/java/hospital/StaffTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StaffTest {
-
     /*
     * Karthik Muthukumar
     * ID: 016281915
-    * */
+    */
+
     //Since staff is abstract we need to extend a subclass to test it
     private static class TestStaff extends Staff{
         public TestStaff(String employeeID, String name, String department, double salary) {
@@ -46,6 +46,71 @@ public class StaffTest {
         //Checks if clockedIn function is true and works
         staff.clockIn();
         assertTrue(staff.isClockedIn());
+    }
+
+    @Test
+    void clockOutAfterClockInIncrementsShiftCount() {
+        //Checks if shiftsworked is incrememnted after one clock in clock out cycle
+
+        staff.clockIn();
+        staff.clockOut();
+
+        assertFalse(staff.isClockedIn());
+        assertEquals(1, staff.getShiftsWorked());
+    }
+
+    @Test
+    void clockOutWithoutClockInDoesNotChangeShiftCount() {
+        //Checks if clockout without clockin doesnt increment the counter shifts worked (it shouldnt)
+
+        staff.clockOut();
+
+        assertFalse(staff.isClockedIn());
+        assertEquals(0, staff.getShiftsWorked());
+    }
+
+    @Test
+    void updateSalaryTest(){
+        staff.updateSalary(125000);
+
+        assertEquals(125000.0, staff.getSalary());
+    }
+
+    @Test
+    void updateSalaryWithNegativeValue(){
+        //Checks if negative salary works (it shouldnt) and makes sure salary remains the same if updated with incorrect values
+        staff.updateSalary(-125000);
+
+        assertEquals(100000.0, staff.getSalary());
+    }
+
+    @Test
+    void assignDepartmentChangesDepartment() {
+        //checks assigndepartment function if it changes the assigned department
+
+        staff.assignDepartment("HR");
+
+        assertEquals("HR", staff.getDepartment());
+    }
+
+    @Test
+    void assignDepartmentInvalidInput(){
+        //checks if empty string input would change department to random empty string (it shouldnt)
+        staff.assignDepartment("    ");
+        assertEquals("Admin", staff.getDepartment());
+    }
+
+    @Test
+    void toStringContainsImportantFields() {
+
+        //Quick test for to string
+        TestStaff staff = new TestStaff("S001", "Karthik", "Admin", 50000);
+
+        String result = staff.toString();
+
+        assertTrue(result.contains("S001"));
+        assertTrue(result.contains("Karthik"));
+        assertTrue(result.contains("Admin"));
     }
 
 
