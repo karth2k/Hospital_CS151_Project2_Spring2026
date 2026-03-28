b414bf1842690d36a595b9df6d103f89edf222d5
Karthik <karthikmuthukumar05@gmail.com>
2026-03-28 12:49:55 -0700

    started adding junit tests starting with checking if constructor works and if clockin works (both successful)

--- src/test/java/hospital/StaffTest.java
+++ src/test/java/hospital/StaffTest.java
@@ -1,9 +1,14 @@
 package hospital;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StaffTest {
 
+    /*
+    * Karthik Muthukumar
+    * ID: 016281915
+    * */
     //Since staff is abstract we need to extend a subclass to test it
     private static class TestStaff extends Staff{
         public TestStaff(String employeeID, String name, String department, double salary) {
@@ -20,5 +25,30 @@ public class StaffTest {
             return "Test Staff";
         }
     }
+
+    //Initializes the staff member after calling constructor
+    TestStaff staff = new TestStaff("S001", "Karthik", "Admin", 100000);
+    @Test
+    void constructorInitializesFieldsCorrectly() {
+
+
+        //Asserts the ID, name, role/department, salary, status, and shiftsworked were initialized properly
+        assertEquals("S001", staff.getEmployeeID());
+        assertEquals("Karthik", staff.getName());
+        assertEquals("Admin", staff.getDepartment());
+        assertEquals(100000.0, staff.getSalary());
+        assertFalse(staff.isClockedIn());
+        assertEquals(0, staff.getShiftsWorked());
+    }
+
+    @Test
+    void clockInSetsClockedInToTrue() {
+        //Checks if clockedIn function is true and works
+        staff.clockIn();
+        assertTrue(staff.isClockedIn());
+    }
+
+
+
 
 }
