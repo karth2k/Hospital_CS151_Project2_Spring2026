98a80f2ffee178c42c160f2c4bab1cb41b449c6b
Karthik <karthikmuthukumar05@gmail.com>
2026-03-28 15:50:17 -0700

    added constructor testing, addpatient, removepatient tests and edge case tests

--- src/test/java/hospital/HospitalTest.java
+++ src/test/java/hospital/HospitalTest.java
@@ -12,14 +12,14 @@ public class HospitalTest {
     private Hospital hospital;
     private Pharmacy pharmacy;
 
+    //makes sure to create initial state before each function is executed so state changes are independent of each other
     @BeforeEach
     void setUp(){
-        //makes sure to create initial state before each function is executed so state changes are independent of each other
         pharmacy = new Pharmacy("CVS-TEST");
         hospital = new Hospital("Dublin Hospital", pharmacy);
     }
     private Patient makePatient(String id, String name) {
-        return new Patient(id, "karthik", 22, "Cold");
+        return new Patient(id, name, 22, "Cold");
     }
     private Doctor makeDoctor(String id, String name) {
         return new Doctor(
@@ -33,6 +33,65 @@ public class HospitalTest {
                 "Day", "Ward A"
         );
     }
+
+    @Test
+    void constructorInitializesFieldsCorrectly() {
+        //makes sure the constructor initializes each field correctly
+        assertEquals("Dublin Hospital", hospital.getHospitalName());
+        assertEquals(0, hospital.getPatientCount());
+        assertEquals(0, hospital.getDoctorCount());
+        assertEquals(0, hospital.getNurseCount());
+        assertEquals(0, hospital.getAppointmentCount());
+        assertEquals(pharmacy, hospital.getPharmacy());
+    }
+
+    @Test
+    void addPatientIncreasesPatientCount() {
+        //makes sure adding the patient to the hospital increases the patient count
+        Patient patient = makePatient("P001", "Karthik");
+
+        hospital.addPatient(patient);
+        assertEquals(1, hospital.getPatientCount());
+        assertEquals(patient, hospital.getPatients()[0]);
+    }
+
+    @Test
+    void addNullPatientDoesNotIncreaseCountTest() {
+        //makes sure if we add a null patient it doesnt increase the count
+        hospital.addPatient(null);
+        assertEquals(0, hospital.getPatientCount());
+    }
+
+    @Test
+    void addDuplicatePatientDoesNotIncreaseCount() {
+        //makes sure that if we add 1 patient and then add another with the same ID it wont add the other one and state they arleady exists
+        //also make sure number of patients isnt incremented
+        Patient p1 = makePatient("P001", "Karthik");
+        Patient p2 = makePatient("P001", "Karthik2");
+
+        hospital.addPatient(p1);
+        hospital.addPatient(p2);
+        assertEquals(1, hospital.getPatientCount());
+    }
+
+    @Test
+    void removePatientDecreasesPatientCount() {
+        //makes sure if a patient is removed the total patient count decreases
+        Patient patient = makePatient("P001", "Karthik");
+
+        hospital.addPatient(patient);
+        hospital.removePatient("P001");
+
+        assertEquals(0, hospital.getPatientCount());
+    }
+
+    @Test
+    void removeMissingPatientThrowsException() {
+        //makes sure it throws an error if a nonexistent patient is removed
+        assertThrows(PatientNotFoundException.class, () -> hospital.removePatient("Donovan Mitchell"));
+    }
+
+
 
 
 
